c999fb3e1eba79049cebfd897aa68bf632ec21aa
Enio Santos <eniostsjr@gmail.com>
2026-04-06 16:12:22 -0300

    iniciando a pagina login

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,9 @@
+import Login from "./pages/login";
+
 function App() {
   return (
     <>
-      <main className="min-h-screen bg-page-gradient text-foreground font-sans">
-        <h1 className="text-red-700">iGendei</h1>
-      </main>
+      <Login />
     </>
   );
 }
