696ddc3d8b1c06c3b41bf624ebcb1c5a5dacde32
Enio Santos <eniostsjr@gmail.com>
2026-04-07 13:33:05 -0300

    finalizando a tela de login

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="flex items-center justify-between px-8 min-h-[68px] bg-(--background-navbar)">
       <h1 className="header-title text-(--foreground) font-(--weight-bold)">
@@ -11,8 +14,8 @@ const Header = () => {
       >
         <p className="text-(--foreground-subtle)">Novo por aqui?</p>
         <a
-          href="#"
-          className="text-(--info) font-(--weight-medium) hover:text-(--info-hover)"
+          onClick={() => navigate("/register")}
+          className="text-(--info) font-(--weight-medium) cursor-pointer hover:text-(--info-hover)"
         >
           Sign up
         </a>
--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import DefaultPageLogin from "./components/layout/defaultPageLogin";
 import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultPageLogin />}>
         <Route index element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   );
