11aa309d720b3374f7d8facdd8f856bbc7c25fe6
Enio Santos <eniostsjr@gmail.com>
2026-04-06 21:22:05 -0300

    finalizando alguns ajustes do footer da page de login

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,39 +2,45 @@ const Footer = () => {
   return (
     <footer
       role="contentinfo"
-      className="flex items-center justify-between px-8 min-h-[--footer-height] bg-(--background-footer)"
+      className="px-8 min-h-[--footer-height] bg-(--background-footer)"
     >
-      <p className="text-(--foreground-subtle) text-sm">
-        © 2026 iGendei Admin Systems. Precision scheduling for the digital
-        architect.
-      </p>
+      <div className="flex justify-between items-center py-4">
+        <div>
+          <h4 className="footer-title text-(--foreground) font-(--weight-semibold)">
+            iGendei Admin Systems
+          </h4>
+          <p className="text-(--foreground-subtle) text-sm">
+            © 2026 | Precision scheduling for the digital architect.
+          </p>
+        </div>
 
-      <nav aria-label="Footer links" className="flex gap-6 text-sm">
-        <a
-          href="#"
-          className="text-(--foreground-link) hover:text-(--info-hover)"
-        >
-          Privacy Policy
-        </a>
-        <a
-          href="#"
-          className="text-(--foreground-link) hover:text-(--info-hover)"
-        >
-          Terms of Service
-        </a>
-        <a
-          href="#"
-          className="text-(--foreground-link) hover:text-(--info-hover)"
-        >
-          Help Center
-        </a>
-        <a
-          href="#"
-          className="text-(--foreground-link) hover:text-(--info-hover)"
-        >
-          Status
-        </a>
-      </nav>
+        <nav aria-label="Footer links" className="flex gap-6 text-sm">
+          <a
+            href="#"
+            className="text-(--foreground-link) hover:text-(--info-hover)"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="text-(--foreground-link) hover:text-(--info-hover)"
+          >
+            Terms of Service
+          </a>
+          <a
+            href="#"
+            className="text-(--foreground-link) hover:text-(--info-hover)"
+          >
+            Help Center
+          </a>
+          <a
+            href="#"
+            className="text-(--foreground-link) hover:text-(--info-hover)"
+          >
+            Status
+          </a>
+        </nav>
+      </div>
     </footer>
   );
 };
