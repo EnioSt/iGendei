4e587452894e26ec3d7a381a566cfc53368ebf5a
Enio Santos <eniostsjr@gmail.com>
2026-04-06 21:38:05 -0300

    iniciando o form da page de login

---
+++ src/components/forms/formLogin/index.jsx
@@ -0,0 +1,19 @@
+const FormLogin = () => {
+  return (
+    <>
+      <section
+        aria-labelledby="page-content"
+        className="w-full max-w-(--container-width)"
+      >
+        <div
+          id="page-content"
+          className="mx-auto w-full max-w-(--login-card-width) rounded-(--radius-md) shadow-(--shadow-md) bg-(--background-card) p-8"
+        >
+          <h2>LULA</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FormLogin;
--- src/components/layout/defaultPageLogin/index.jsx
+++ src/components/layout/defaultPageLogin/index.jsx
@@ -11,17 +11,7 @@ const DefaultPageLogin = () => {
         role="main"
         className="flex flex-1 items-center justify-center px-6 py-12 bg-(--background-secondary)"
       >
-        <section
-          aria-labelledby="page-content"
-          className="w-full max-w-(--container-width)"
-        >
-          <div
-            id="page-content"
-            className="mx-auto w-full max-w-(--login-card-width) rounded-(--radius-md) shadow-(--shadow-md) bg-(--background-card) p-8"
-          >
-            <Outlet />
-          </div>
-        </section>
+        <Outlet />
       </main>
 
       <Footer />
